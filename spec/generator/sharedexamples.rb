b59f6817b65eff6ce18bb52e8fbe20dbdf5a6ab1
Kevin Fischer <dertraveler@gmail.com>
2015-12-10 18:51:56 +0900

    Added each and variant method to Generator class

--- spec/generator/sharedexamples.rb
+++ spec/generator/sharedexamples.rb
@@ -27,5 +27,29 @@ module RubyQuickCheck
         expect(second_generated).to eq first_generated
       end
     end
+
+    describe "#each" do
+
+      it "yields the same arguments it would also generate" do
+        seeded_rng = Random.new(seed)
+        values = (1..size).map { subject.generate(seeded_rng) }
+        seeded_rng = Random.new(seed)
+        expect { |b| subject.each(seeded_rng, size, &b) }.to yield_successive_args(*values)
+      end
+
+    end
+
+    describe "#variant" do
+
+      it "generates different values even with the same generator" do
+        seeded_rng = Random.new(seed)
+        first_generated = (1..size).map { subject.generate(seeded_rng) }
+        variant_gen = subject.variant(1)
+        seeded_rng = Random.new(seed)
+        second_generated = (1..size).map { variant_gen.generate(seeded_rng) }
+        expect(second_generated).not_to eq first_generated
+      end
+
+    end
   end
 end
--- src/generator.rb
+++ src/generator.rb
@@ -17,16 +17,61 @@ module RubyQuickCheck
 
     STANDARD_SIZE = 30
 
+    Contract RespondTo[:__gen__], HashOf[Symbol, Any] => Proc
     ##
     # Takes any class or object that defines a __gen__ method describing how to
     # generate a value of a certain size (if applicable for the value type).
     # Can also pass on a hash with further options to that method.
-    Contract RespondTo[:__gen__], HashOf[Symbol, Any] => Proc
     def initialize(generable, options = {})
       @size = STANDARD_SIZE
       define_singleton_method(:generate) do |rng = Random.new|
         generable.__gen__(@size, options).call(rng)
       end
+    end
+
+    Contract Random => Enumerator
+    ##
+    # :call-seq:
+    #   each(rng) -> Enumerator
+    #   each(rng) {block}
+    #   each(rng, size) -> Enumerator
+    #   each(rng, size) {block}
+    #
+    # Yields the values generated to the provided _block_.
+    # If no _size_ is specified an unlimited number of values will be yielded.
+    # If no _block_ is specified an enumerator will be returned.
+    def each(rng = Random.new)
+      enum_for(:each, rng)
+    end
+
+    Contract Random, Proc => Any
+    def each(rng = Random.new)
+      loop { yield generate(rng) }
+    end
+
+    Contract Random, Nat => Enumerator
+    def each(rng = Random.new, size)
+      enum_for(:each, rng, size)
+    end
+
+    Contract Random, Nat, Proc => Any
+    def each(rng = Random.new, size)
+      size.times { yield generate(rng) }
+    end
+
+    Contract Integer => Generator
+    ##
+    # Returns a generator that will create different values even when using the
+    # exact same Random number generator.
+    def variant(perturb_factor)
+      result = clone
+      old_method = method(:generate)
+      result.define_singleton_method(:generate) do |rng|
+        perturb_factor.times { rng.rand }
+        old_method.call(rng)
+      end
+
+      result
     end
 
     protected
